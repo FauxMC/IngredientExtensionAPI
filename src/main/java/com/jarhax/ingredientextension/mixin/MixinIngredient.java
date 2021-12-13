c47b961d647eb1e610ae38b0d28bc33f9d10c9ed
Tyler Hancock <darklime@live.ca>
2021-12-12 20:53:47 -0700

    Add JavaDoc to explain the mixins and changes to the vanilla network spec.

--- src/main/java/com/jarhax/ingredientextension/mixin/MixinIngredient.java
+++ src/main/java/com/jarhax/ingredientextension/mixin/MixinIngredient.java
@@ -17,39 +17,67 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Ingredient.class)
 public class MixinIngredient {
-    
+
+    /**
+     * This Mixin patches {@link Ingredient#toNetwork(FriendlyByteBuf)} to add support for expanded ingredients. This is
+     * done by modifying the network specification. All ingredients will have an additional Integer written to the
+     * buffer. This value is used as a flag to determine what serialization method should be used.
+     * <p>
+     * Ingredients managed by this API will have a flag value of {@value Constants#NETWORK_MARKER_EXTENDED}. This flag
+     * will always be followed by a UTF-8 String that contains the registry ID of the serializer that manages this type
+     * of ingredient. Additional data may be encoded by the serializer and is specific to each serializer
+     * implementation.
+     * <p>
+     * Vanilla ingredients and Ingredients not managed by this API will have the flag value of {@value
+     * Constants#NETWORK_MARKER_VANILLA}. When this flag is encountered we hand network serialization back to the
+     * vanilla game.
+     */
     @Inject(method = "toNetwork", at = @At("HEAD"), cancellable = true)
     private void toNetwork(FriendlyByteBuf buf, CallbackInfo ci) {
-        
+
         final Ingredient self = (Ingredient) (Object) this;
-        
+
         // Extended Ingredient
-        if(self instanceof IngredientExtendable extended) {
-            
+        if (self instanceof IngredientExtendable extended) {
+
             final IIngredientSerializer<?> serializer = extended.getSerializer();
             final ResourceLocation serializerId = IngredientHelper.INGREDIENT_SERIALIZER_REGISTRY.getKey(serializer);
-            
+
             self.dissolve();
             buf.writeInt(Constants.NETWORK_MARKER_EXTENDED);
             buf.writeUtf(serializerId.toString());
             ((IIngredientSerializer) serializer).toNetwork(buf, self);
             ci.cancel();
         }
-        
+
         // Externally Managed Ingredient
         else {
-            
+
             buf.writeInt(Constants.NETWORK_MARKER_VANILLA);
         }
     }
-    
+
+    /**
+     * This Mixin patches {@link Ingredient#fromNetwork(FriendlyByteBuf)} to add support for expanded ingredients. This
+     * is done by modifying the network specification. All ingredients will have an additional Integer written to the
+     * buffer. This value is used as a flag to determine what serialization method should be used.
+     * <p>
+     * Ingredients managed by this API will have a flag value of {@value Constants#NETWORK_MARKER_EXTENDED}. This flag
+     * will always be followed by a UTF-8 String that contains the registry ID of the serializer that manages this type
+     * of ingredient. Additional data may be encoded by the serializer and is specific to each serializer
+     * implementation.
+     * <p>
+     * Vanilla ingredients and Ingredients not managed by this API will have the flag value of {@value
+     * Constants#NETWORK_MARKER_VANILLA}. When this flag is encountered we hand network serialization back to the
+     * vanilla game.
+     */
     @Inject(method = "fromNetwork", at = @At("HEAD"), cancellable = true)
     private static void fromNetwork(FriendlyByteBuf friendlyByteBuf, CallbackInfoReturnable<Ingredient> cir) {
-        
+
         final int marker = friendlyByteBuf.readInt();
-        
+
         // Handle extended ingredients
-        if(marker == Constants.NETWORK_MARKER_EXTENDED) {
+        if (marker == Constants.NETWORK_MARKER_EXTENDED) {
 
             final ResourceLocation typeId = ResourceLocation.tryParse(friendlyByteBuf.readUtf());
             final IIngredientSerializer<?> serializer = IngredientHelper.getSerializer(typeId);
@@ -66,31 +94,46 @@ public class MixinIngredient {
                 throw new RuntimeException(errorMessage);
             }
         }
-        
+
         // Handle non-vanilla ingredients
-        else if(marker != Constants.NETWORK_MARKER_VANILLA) {
-            
+        else if (marker != Constants.NETWORK_MARKER_VANILLA) {
+
             final String message = "Failed to deserialize ingredient! Expected a marker with value '" + Constants.NETWORK_MARKER_EXTENDED + "' or '" + Constants.NETWORK_MARKER_VANILLA + "'. Got '" + marker + "' instead.";
             Constants.LOGGER.error(message);
             throw new RuntimeException(message);
         }
     }
-    
+
+    /**
+     * This Mixin patches {@link Ingredient#fromJson(JsonElement)} to add support for additional Ingredient types. If
+     * the JSON data is an object and contains a type property we will attempt to match the value of that type to an
+     * Ingredient serializer using {@link IngredientHelper#getSerializer(ResourceLocation)}. If the ID matches up to a
+     * registered serializer the deserialization process will be handed off to them.
+     * <p>
+     * It is conceivable that other mods and frameworks may wish to use the type property for their own unrelated
+     * systems. In these scenarios the warning of an unknown ingredient type will be undesirable. This warning can be
+     * disabled by including a boolean with the name {@literal ignoreIngredientExtensionAPI} and the value {@literal
+     * true}.
+     */
     @Inject(method = "fromJson(Lcom/google/gson/JsonElement;)Lnet/minecraft/world/item/crafting/Ingredient;", at = @At("HEAD"), cancellable = true)
     private static void fromJson(JsonElement jsonElement, CallbackInfoReturnable<Ingredient> callback) {
-        
-        if(jsonElement instanceof JsonObject jsonObj && jsonObj.has("type")) {
-            
+
+        // Only process JSON objects with a type property.
+        if (jsonElement instanceof JsonObject jsonObj && jsonObj.has("type")) {
+
             final ResourceLocation typeId = new ResourceLocation(jsonObj.get("type").getAsString());
             final IIngredientSerializer<?> serializer = IngredientHelper.getSerializer(typeId);
-            
-            Constants.LOGGER.info(serializer);
-            if(serializer != null) {
-                
+
+            // If the type matches up to a known serializer hand off the deserialization.
+            if (serializer != null) {
+
                 final Ingredient out = serializer.fromJson(jsonObj);
                 callback.setReturnValue(out);
-            } else if(!jsonObj.has("ignoreIngredientExtensionAPI")) {
-                
+            }
+
+            // If the type does not match up attempt to post a warning.
+            else if (!jsonObj.has("ignoreIngredientExtensionAPI")) {
+
                 Constants.LOGGER.warn("Tried to read an ingredient of unknown type {}.", typeId);
             }
         }
--- src/main/java/com/jarhax/ingredientextension/mixin/MixinShapelessRecipe.java
+++ src/main/java/com/jarhax/ingredientextension/mixin/MixinShapelessRecipe.java
@@ -1,5 +1,6 @@
 package com.jarhax.ingredientextension.mixin;
 
+import com.jarhax.ingredientextension.api.ingredient.IngredientExtendable;
 import com.jarhax.ingredientextension.api.ingredient.IngredientHelper;
 import net.minecraft.core.NonNullList;
 import net.minecraft.world.inventory.CraftingContainer;
@@ -16,37 +17,55 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ShapelessRecipe.class)
 public abstract class MixinShapelessRecipe {
-    
+
     @Shadow
     public abstract NonNullList<Ingredient> getIngredients();
-    
+
     @Shadow
     @Final
     private NonNullList<Ingredient> ingredients;
-    
-    
+
+    /**
+     * This Mixin patches {@link ShapelessRecipe#matches(CraftingContainer, Level)} to account for non-vanilla
+     * ingredients. The vanilla code for shapeless recipes use cached properties such as {@link
+     * Ingredient#getStackingIds()} rather than testing ingredients with their {@link Ingredient#test(ItemStack)}
+     * method. This behavior is undesirable for custom ingredients which rely on testing for things like ItemStack
+     * sensitive matching.
+     * <p>
+     * These issues are circumvented by replacing the vanilla logic with new logic that uses a Bipartite graph. This
+     * logic will only be used if one or more ingredients in the recipe are non-standard ingredients that return true
+     * from their {@link IngredientExtendable#requiresTesting()} method.
+     * <p>
+     * This new logic has been tested for compatibility with vanilla ingredients and recipes. The performance
+     * characteristics are also comparable to the vanilla implementation.
+     */
     @Inject(method = "matches(Lnet/minecraft/world/inventory/CraftingContainer;Lnet/minecraft/world/level/Level;)Z", at = @At("HEAD"), cancellable = true)
     public void matches(CraftingContainer craftingContainer, Level level, CallbackInfoReturnable<Boolean> cir) {
-        
-        if(!IngredientHelper.requiresTesting(this.ingredients)) {
+
+        // Only use our custom logic if it is required.
+        if (!IngredientHelper.requiresTesting(this.ingredients)) {
+
             return;
         }
-        NonNullList<ItemStack> containerItems = NonNullList.create();
+
+        final NonNullList<ItemStack> containerItems = NonNullList.create();
         int itemCount = 0;
-        for(int i = 0; i < craftingContainer.getContainerSize(); ++i) {
-            ItemStack itemStack = craftingContainer.getItem(i);
-            if(!itemStack.isEmpty()) {
+
+        for (int i = 0; i < craftingContainer.getContainerSize(); ++i) {
+
+            final ItemStack itemStack = craftingContainer.getItem(i);
+
+            if (!itemStack.isEmpty()) {
+
                 itemCount++;
                 containerItems.add(itemStack);
             }
-            
-        }
-        
-        if(itemCount == this.ingredients.size()) {
 
-            // We could return false if the count is not good, but it may mess with other mixins
+        }
+
+        if (itemCount == this.ingredients.size()) {
+
             cir.setReturnValue(IngredientHelper.testUnorderedMatch(this.getIngredients(), containerItems));
         }
-
     }
 }
